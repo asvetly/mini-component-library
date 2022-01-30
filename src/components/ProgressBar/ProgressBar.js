9eaa87ab6b3d92c3b5cff79379bc2c6157aa99e7
Светличный Александр Викторович <asvetly1337@gmail.com>
2022-01-30 15:04:05 +0300

    Add ProgressBar component

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,70 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const STYLES = {
+    small: {
+        height: 8,
+        padding: 0,
+        radius: 4,
+    },
+    medium: {
+        height: 12,
+        padding: 0,
+        radius: 4,
+    },
+    large: {
+        height: 16,
+        padding: 4,
+        radius: 8,
+    },
+};
+
+const ProgressBarWrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0 2px 4px ${COLORS.transparentGray35};
+  border-radius: var(--radius);
+  padding: var(--padding);
+`;
+
+const BarWrapper = styled.div`
+  border-radius: 4px;
+  overflow: hidden;
+`;
+
+const Bar = styled.div`
+  width: var(--width);
+  height: var(--height);
+  background-color: ${COLORS.primary};
+  border-radius: 4px 0 0 4px;
+  will-change: width;
+  transition: width 100ms ease-out;
+`;
+
+const ProgressBar = ({ value = 0, minValue = 0, maxValue = 100, size }) => {
+    const styles = STYLES[size];
+
+    return (
+        <ProgressBarWrapper
+            role="progressbar"
+            aria-valuenow={value}
+            aria-valuemin={ minValue }
+            aria-valuemax={ maxValue }
+            style={{
+                '--padding': styles?.padding + 'px',
+                '--radius': styles?.radius + 'px',
+            }}
+        >
+            <VisuallyHidden>{ (value / maxValue) * 100 }%</VisuallyHidden>
+            <BarWrapper>
+                <Bar
+                    style={{
+                        '--width': `${(value / maxValue) * 100}%`,
+                        '--height': styles?.height + 'px',
+                    }}
+                />
+            </BarWrapper>
+        </ProgressBarWrapper>
+    );
 };
 
 export default ProgressBar;
